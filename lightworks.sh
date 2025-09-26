a486733a2e545d39c3b45a31d7ec8266b260fbd8
Nick Walsh <nick@lwks.com>
2025-09-26 09:54:41 +0100

    Update to Decklink SDK 15.1, copy only one set of libc++ files, add note

--- lightworks.sh
+++ lightworks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#Launch the app
+#Launch the app (n.b. could move the environment variables to the .yaml file with the other env vars)
 GDK_BACKEND=x11 GDK_SCALE=1 /app/lib/lightworks/ntcardvt
